bd6ee7912d9ba83451f87498b6b4b4a25219c36c
ReDay Zarra <redayzarra@gmail.com>
2022-12-22 20:25:59 -0500

    Adding notes on objects in R

---
+++ objects.R
@@ -0,0 +1,12 @@
+# In R, you can assign variables by using the assign symbol
+a <- 1
+b <- 1
+c <- -1
+
+print(a) # Allows us to print the variable
+
+ls() # Shows us all the variables saved in the work space (environment)
+
+(-b + sqrt(b^2 - 4*a*c)) / (2*a)
+
+(-b - sqrt(b^2 - 4*a*c)) / (2*a)
