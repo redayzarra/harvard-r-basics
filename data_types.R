327f440c38bcb9dbebbcdf3db4ec08c89a63e60b
ReDay Zarra <redayzarra@gmail.com>
2022-12-31 20:26:52 -0500

    Fixing saving issue

---
+++ data_types.R
@@ -0,0 +1,10 @@
+# Variables can have different types. The 'class' function can help us determine the type of an object.
+
+a <- 2
+class(a) # The result confirms that a is numeric
+
+class(ls) # The result confirms that ls is a function
+
+
+# The most common way of storing data sets is with data frames
+
