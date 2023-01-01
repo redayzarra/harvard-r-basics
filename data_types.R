d80f0db0ded1568dd1d38cc412525d6e70113a7c
ReDay Zarra <redayzarra@gmail.com>
2022-12-31 21:01:21 -0500

    Adding notes on character vectors

--- data_types.R
+++ data_types.R
@@ -10,7 +10,6 @@ class(ls) # The result confirms that ls is a function
 # Think of data frames as data tables:
 #   - Rows are like observations
 #   - Columns are like different variables
-
 # Data frames are useful for data sets because we can combine different types into one single object
 
 library(dslabs) # Importing the dslabs library
@@ -33,10 +32,24 @@ murders$population # Gives us the column associated with the population label, g
 
 names(murders) # Gives us the names of the five columns or variables, we can use accessor($) to access those columns
 
-
 pop <- murders$population # Assigns the variable pop to the column labeled population
 
 length(pop) # Gives us the length of the variable pop which is 51
 
 class(pop) # Tells us the class of pop which is a numeric because it stores numbers
+
+
+# Character vectors are distinguished from variables and character strings from "quotes"
+# All entries in a character vector need to be characters
+
+b <- 1
+b # Returns the number 1
+
+b <- "b"
+b # Now it returns the character "b" not the variable
+
+class(murders$state) # Returns character because the state column has all character strings for values
+
+
+# Logical vectors
 
