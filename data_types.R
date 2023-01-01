7fd42b4ca4a76220da77973dd3e838e9f9827a39
ReDay Zarra <redayzarra@gmail.com>
2022-12-31 20:36:02 -0500

    Importing dslabs murders dataframe, acknowledging bugs

--- data_types.R
+++ data_types.R
@@ -6,5 +6,13 @@ class(a) # The result confirms that a is numeric
 class(ls) # The result confirms that ls is a function
 
 
-# The most common way of storing data sets is with data frames
+# The most common way of storing data sets is with data frames:
+# Think of data frames as data tables:
+#   - Rows are like observations
+#   - Columns are like different variables
 
+# Data frames are useful for data sets because we can combine different types into one single object
+
+library(dslabs)
+data(murders)
+class(murders)
