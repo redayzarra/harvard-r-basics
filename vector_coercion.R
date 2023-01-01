50da808f7f1b643c8dcecdd7890c392bc9bcf3f4
ReDay Zarra <redayzarra@gmail.com>
2023-01-01 11:43:17 -0500

    Exploring functions for data coercion

--- vector_coercion.R
+++ vector_coercion.R
@@ -4,4 +4,14 @@ x <- c(1, "canda", 3)
 
 class(x) # We should expect to see an error here, because the vector isn't numeric or character, but we get character
 
-x # R has automatically converted the numbers we assigned into character strings
+x # R has automatically converted the numbers we assigned into character strings. R coerced the data into character strings.
+
+
+# There are many functions to force a specific data coercion
+
+y <- 1:5 # Generates number 1 through 5
+z <- as.character(y) # Can turn numbers into characters 
+z # Stores a copy of the numeric vector y with character strings from 1 to 5
+
+a <- as.numeric(z) # Can turn characters back into numbers
+a # Stores a copy of vector z but all the number character strings are actually numbers again
