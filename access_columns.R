28234e15a3bc041806778ad53fdf43980073d1db
ReDay Zarra <redayzarra@gmail.com>
2023-01-02 17:22:15 -0500

    Finishing lesson on accessing numeric values from datasets

---
+++ access_columns.R
@@ -0,0 +1,11 @@
+# To get numeric values from functions, you can use the accessor ($) or the pull function
+
+us_murder_rate <- murders %>% summarize(rate = sum(total) / sum(population) * 100000) # Establishing the murder rate variable
+
+us_murder_rate %>% pull(rate) # us_murder_rate is a dataset, but using pull() we have pulled the numeric value out of the dataset
+
+us_murder_rate <- murders %>%
+  summarize(rate = sum(total) / sum(population) * 100000) %>%
+  pull(rate)
+
+us_murder_rate # The object is no longer a dataset, it is now a numeric value that we can work with in other functions
